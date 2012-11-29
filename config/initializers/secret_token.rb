8806ce9550e31570b229424ed124cd04e0dc4e08
Nicholas Yoon <s11022ev@gmail.com>
2012-11-29 12:38:09 -0500

    Revert "Initial Project Commit"
    This reverts commit 8cc6f3e8cf69ed2bb3a980ef3e9a4f08e7684175.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ITWebsite::Application.config.secret_token = '39eefdfca07fdab8777d41814b5a164ad695a8ad5ad8c460477670ee8a9fafcc4b03fa7036b7a9cb45ab5dc986081f239727470035a9f16b62d5ddce3c26fb64'
